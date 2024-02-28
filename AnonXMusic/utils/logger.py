069de1fe072de59a41e7b5c1dfc19e36edf2da23
testingslur <136461439+testingslur@users.noreply.github.com>
2024-02-28 23:36:59 +0700

    Update logger.py

--- AnonXMusic/utils/logger.py
+++ AnonXMusic/utils/logger.py
@@ -10,16 +10,17 @@ async def play_logs(message, streamtype):
         logger_text = f"""
 <b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
 
-<b>ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
-<b>ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> {message.chat.title}
-<b>ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.chat.username}
+<b>💬 ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
+<b>💬 ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> {message.chat.title}
+<b>💬 ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.chat.username}
 
-<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>
-<b>ɴᴀᴍᴇ :</b> {message.from_user.mention}
-<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}
+<b>👨🏻‍🚀 ᴜsᴇʀ {message.from_user.mention}
+<b>🔖 ɴᴀᴍᴇ :</b> {message.from_user.mention}
+<b>🔖 ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}
+<b>🆔 ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>
 
-<b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
-<b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}"""
+<b>🔖 ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
+<b>💽 sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}"""
         if message.chat.id != LOGGER_ID:
             try:
                 await app.send_message(
